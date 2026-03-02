31a9130373dd5b69284fb2050d37b49af402d12c
EkoAsif <ekoasif45@gmail.com>
2026-03-02 19:54:50 +0700

    version 4.8

--- asstets/script.js
+++ asstets/script.js
@@ -2,26 +2,7 @@
 const menuToggler = document.querySelector('.menu-toggler');
 const sideBar = document.querySelector('.side-bar');
 
-// page navigation variables
-const navItemLinks = document.querySelectorAll('.nav li a');
-const pages = document.querySelectorAll('.page');
-
-
-// variables for filtering
-// const filterBtn = document.querySelectorAll('.filter-item');
-// const itemCategory = document.querySelectorAll('.item-category');
-// =================================atas ori
-// const filterBtn = document.querySelectorAll('.filter-item');
-// const portfolioItems = document.querySelectorAll('.portfolio-item');
-
-// // toggling sidebar in mobile
-// menuToggler.addEventListener('click', function(){
-//   sideBar.classList.toggle('active');
-// });
-
-
 // page navigation functionality
-
 for (let i = 0; i < navItemLinks.length; i++) {
   // added onclick event in nav links
   navItemLinks[i].addEventListener('click', function(){
@@ -98,33 +79,3 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 });
 
-
-
-// // added eventListener in filter buttons
-// for (let i = 0; i < filterBtn.length; i++) {
-//   filterBtn[i].addEventListener('click', function(){
-
-//     // remove all active class from filter button
-//     for (let i = 0; i < filterBtn.length; i++) {
-//       filterBtn[i].classList.remove('active');
-//     }
-//     // added active class on filter button clicked
-//     this.classList.add('active');
-
-//     // show item, based on filter button click
-//     for (let i = 0; i < itemCategory.length; i++) {
-//       const itemCategoryText = itemCategory[i].textContent;
-//       console.log(itemCategoryText);
-//       switch (this.textContent) {
-//         case itemCategoryText:
-//           itemCategory[i].parentElement.classList.add('active');
-//           break;
-//         case 'All':
-//           itemCategory[i].parentElement.classList.add('active');
-//           break;
-//         default:
-//           itemCategory[i].parentElement.classList.remove('active');
-//       }
-//     }
-//   });
-// }
